42856f233bdf2287e03e535e30c3e4328ac87ecf
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-05-30 12:15:25 +0530

    Add Chat UI

--- sivalabs-mcp-client/src/main/java/com/sivalabs/mcpclient/VideoController.java
+++ sivalabs-mcp-client/src/main/java/com/sivalabs/mcpclient/VideoController.java
@@ -2,9 +2,7 @@ package com.sivalabs.mcpclient;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class VideoController {
