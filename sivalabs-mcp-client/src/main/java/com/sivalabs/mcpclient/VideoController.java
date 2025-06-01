85e8166142782d3dade670cdbe7d59b598b6614c
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-06-01 09:55:53 +0530

    Updated videos list and formatting the response

--- sivalabs-mcp-client/src/main/java/com/sivalabs/mcpclient/VideoController.java
+++ sivalabs-mcp-client/src/main/java/com/sivalabs/mcpclient/VideoController.java
@@ -1,11 +1,14 @@
 package com.sivalabs.mcpclient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class VideoController {
+    private static final Logger log = LoggerFactory.getLogger(VideoController.class);
     private final ChatClient chatClient;
 
     public VideoController(ChatClient.Builder chatClientBuilder,
@@ -18,10 +21,38 @@ public class VideoController {
 
     @PostMapping("/ask")
     public Answer ask(@RequestBody Question question) {
-        return chatClient.prompt()
+        var answer = chatClient.prompt()
                 .user(question.question())
                 .call()
                 .entity(Answer.class);
+        log.debug("Answer from AI: {}", answer);
+        String markdownAnswer = formatResponse(question, answer);
+        log.debug("Markdown formatted answer: {}", markdownAnswer);
+        var htmlResponse = MarkdownHelper.toHTML(markdownAnswer);
+        log.debug("HTML formatted answer: {}", htmlResponse);
+        return new Answer(htmlResponse);
+    }
+
+    private String formatResponse(Question question, Answer answer) {
+        var prompt = """
+                Following are the question and answer:
+                
+                Question: {question}
+                
+                Answer: {answer}
+                
+                Answer is in JSON format.
+                
+                Format the answer in plain text with Markdown syntax and return only the formatted response.
+                """;
+        return chatClient
+                .prompt()
+                .user(u -> u.text(prompt)
+                        .param("question", question.question())
+                        .param("answer", answer.answer())
+                )
+                .call()
+                .content();
     }
 
     public record Question(String question) {
